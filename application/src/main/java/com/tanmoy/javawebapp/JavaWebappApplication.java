9aa29852ea04423e5c81e2d313226e9089109edd
TanmoyDas02 <tanmoy021997@gmail.com>
2026-07-11 13:51:16 +0530

    feat: initialize Spring Boot WAR application with home endpoint

---
+++ application/src/main/java/com/tanmoy/javawebapp/JavaWebappApplication.java
@@ -0,0 +1,13 @@
+package com.tanmoy.javawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebappApplication.class, args);
+	}
+
+}
